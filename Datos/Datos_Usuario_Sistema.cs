daf0d7030b0ce0be5594615b016ac52717434592
brand <brand@JK>
2023-10-09 16:58:26 -0500

    Agregar archivos de proyecto.

---
+++ Datos/Datos_Usuario_Sistema.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Entidad
+
+namespace Datos
+{
+    public class Datos_Usuario_Sistema
+    {
+        // Conexion a la BD sistema_comercial
+        SqlConnection cn_bdprueba = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+
+        // Implementar los metodos de la clase Usuario_Sistema
+        public String Validar(Usuario_Sistema oUserSistema)
+        {
+
+            // Ejecutar el sp_ActualizarCliente -> update -> No devuelve registros
+            cn_bdprueba.Open();
+            SqlCommand cmd = new SqlCommand("sp_Validar_Usuario_Sistema", cn_bdprueba);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@nom", oUserSistema.Nombre);
+            cmd.Parameters.AddWithValue("@pwd", oUserSistema.Password);
+
+            SqlParameter mensaje = new SqlParameter("@resultado", SqlDbType.VarChar, 100);
+            mensaje.Direction = ParameterDirection.Output;
+            cmd.Parameters.Add(mensaje);
+
+            cmd.ExecuteNonQuery();
+            cn_bdprueba.Close();
+            return cmd.Parameters["@resultado"].Value.ToString();
+        }
+}
---
+++ Entidad/Entidad_Usuario_Sistema.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    internal class Entidad_Usuario_Sistema
+    {
+        public String Nombre { get; set; }
+        public String Password { get; set; }
+        public DateTime Fecha { get; set; }
+        public Boolean Estado { get; set; }
+    }
+}
